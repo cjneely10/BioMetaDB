84756370be053fd3081933eaa084108ec0421cfe
cjneely10 <Christopher.neely1200@gmail.com>
2019-07-11 18:04:11 -0700

    RecordList join in progress

--- BioMetaDB/DataStructures/record_list.pyx
+++ BioMetaDB/DataStructures/record_list.pyx
@@ -261,7 +261,7 @@ cdef class RecordList:
         :return:
         """
         try:
-            self.results = self.sess.query(self.TableClass).join(other.TableClass._id).all()
+            self.results = self.sess.query(self.TableClass).join(other._id).all()
         # Column name not found
         except OperationalError:
             raise ColumnNameNotFoundError
