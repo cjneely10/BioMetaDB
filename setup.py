01332cd0866874ca96bbea173b71cffe61ad82e5
Christopher Neely <christopher.neely1200@gmail.com>
2019-08-20 22:13:20 -0700

    Remove unneeded vars

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 import os
 from distutils.core import setup
 from Cython.Build import cythonize
-here = os.path.abspath(os.path.dirname(__file__))
-with open(os.path.join(here, 'README.md'), "r") as f:
+with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), "r") as f:
     long_description = f.read()
 setup(
-    name='BioMetaDB',    # This is the name of your PyPI-package.
+    name='BioMetaDB',
     version='0.1.0',
     description='Use biological data to generate SQL database schema',
     url="https://github.com/cjneely10/BioMetaDB",
