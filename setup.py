f6b8c6f9f4922c275a0a542d38fc3a548079c5c2
Christopher Neely <christopher.neely1200@gmail.com>
2019-10-13 17:10:27 -0700

    Prepare for pip installation

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 import os
+import setuptools
 from distutils.core import setup
 from Cython.Build import cythonize
 with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), "r") as f:
     long_description = f.read()
 setup(
     name='BioMetaDB',
-    version='0.1.0',
+    version='0.1.1',
     description='Use biological data to generate SQL database schema',
     url="https://github.com/cjneely10/BioMetaDB",
     author="Christopher Neely",
