851020a9f1f2a321c71355a05b8392a8344f2a67
Christopher Neely <christopher.neely1200@gmail.com>
2020-01-09 15:24:49 -0800

    Begin massive refacotr

--- BioMetaDB/Serializers/fix_file.pyx
+++ BioMetaDB/Serializers/fix_file.pyx
@@ -30,8 +30,65 @@ cdef class FixFile:
         cdef str py_header_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),
                                                "fix_header.txt")
         cdef object fix_header_ptr = open(py_header_path, "rb")
-        for line in fix_header_ptr:
-            cfile.write(line.decode())
+        fix_header_ptr.write(b"""@ All header lines are statically generated and are preceded by '@'
+            @
+            @   This file was created by using `dbdm INTEGRITY`
+            @   .fix file structure identifies issues in your project and displays them as:
+            @
+            @       ISSUE
+            @       <DATA_TYPE>:    <ID>
+            @       <ISSUE_TYPE>  <FIX_TYPE>
+            @       ---
+            @   Note that all .fix files are tab delimited
+            @
+            @
+            @ Depending on the issues identified, your .fix file may contain the following:
+            @
+            @       PROJECT:    <PROJECT_NAME>
+            @       INVALID_PATH    NONE
+            @
+            @
+            @       RECORD:    <RECORD_ID>:  <TABLE_NAME>
+            @       BAD_TYPE    NONE
+            @
+            @       RECORD:    <RECORD_ID>:  <TABLE_NAME>
+            @       BAD_LOCATION    DELETE
+            @
+            @
+            @       FILE:    <FILE_ID>:  <TABLE_NAME>
+            @       BAD_RECORD    NONE
+            @
+            @       Each issue is defined as:
+            @
+            @       PROJECT INVALID_PATH:   working_dir path stored in config file is invalid
+            @       RECORD  BAD_TYPE:       data type for record could not be determined
+            @       RECORD  BAD_LOCATION:       stored file path for record does not exist
+            @       FILE    BAD_RECORD:     file exists in project directory with no assigned database id
+            @
+            @   For each issue, default actions are presented. The following fixes are available:
+            @
+            @       PROJECT:    <PROJECT_NAME>
+            @       INVALID_PATH    PATH    /path/to/project_directory
+            @
+            @
+            @       RECORD:    <RECORD_ID>: <TABLE_NAME>
+            @       BAD_TYPE    SET     data_type(e.g. fasta/fastq)
+            @
+            @       RECORD:    <RECORD_ID>: <TABLE_NAME>
+            @       BAD_LOCATION    FILE    /path/to/data_file.fna
+            @
+            @
+            @       FILE:    <FILE_ID>: <TABLE_NAME>
+            @       BAD_RECORD    RECORD    record_id_in_database
+            @
+            @   If you do not wish to fix a particular issue, simple delete the entire entry,
+            @   starting from "ISSUE" until "---"
+            @
+            @   Do NOT add additional characters, newline or otherwise
+            @
+            """)
+        # for line in fix_header_ptr:
+        #     cfile.write(line.decode())
         self.fp = cfile
         fix_header_ptr.close()
 
