7e505572e4d61483f6699ccdfc540fbfdddee5b7
Christopher Neely <christopher.neely1200@gmail.com>
2020-01-09 20:06:50 -0800

    Add tracked .c build files; Create build option for cythonizing (ease of pip build)

--- setup.py
+++ setup.py
@@ -5,23 +5,45 @@ from setuptools import setup, Extension
 with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), 'README.md'), "r") as f:
     long_description = f.read()
 
-# with open(os.path.join(os.path.abspath(os.path.dirname(__file__)), "requirements.txt"), "r") as fp:
-#     install_requires = fp.read().strip().split("\n")
+
+# https://cython.readthedocs.io/en/latest/src/userguide/source_files_and_compilation.html#distributing-cython-modules
+def no_cythonize(extensions, **_ignore):
+    for extension in extensions:
+        sources = []
+        for sfile in extension.sources:
+            path, ext = os.path.splitext(sfile)
+            if ext in (".pyx", ".py"):
+                if extension.language == "c++":
+                    ext = ".cpp"
+                else:
+                    ext = ".c"
+                sfile = path + ext
+            sources.append(sfile)
+        extension.sources[:] = sources
+    return extensions
+
+
+CYTHONIZE = bool(int(os.getenv("CYTHONIZE", 0)))
 
 extensions = [
-    Extension("BioMetaDB.Accessories.arg_parse", ["BioMetaDB/Accessories/arg_parse.c"]),
-    Extension("BioMetaDB.Accessories.bio_ops", ["BioMetaDB/Accessories/bio_ops.c"]),
-    Extension("BioMetaDB.Accessories.ops", ["BioMetaDB/Accessories/ops.c"]),
-    Extension("BioMetaDB.Accessories.program_caller", ["BioMetaDB/Accessories/program_caller.c"]),
-    Extension("BioMetaDB.DataStructures.record_list", ["BioMetaDB/DataStructures/record_list.c"]),
-    Extension("BioMetaDB.DBManagers.class_manager", ["BioMetaDB/DBManagers/class_manager.c"]),
-    Extension("BioMetaDB.DBManagers.integrity_manager", ["BioMetaDB/DBManagers/integrity_manager.c"]),
-    Extension("BioMetaDB.DBManagers.update_manager", ["BioMetaDB/DBManagers/update_manager.c"]),
-    Extension("BioMetaDB.Models.functions", ["BioMetaDB/Models/functions.c"]),
-    Extension("BioMetaDB.Serializers.count_table", ["BioMetaDB/Serializers/count_table.c"]),
-    Extension("BioMetaDB.Serializers.fix_file", ["BioMetaDB/Serializers/fix_file.c"]),
-    Extension("BioMetaDB.Serializers.tsv_joiner", ["BioMetaDB/Serializers/tsv_joiner.c"]),
+    Extension("BioMetaDB.Accessories.arg_parse", ["BioMetaDB/Accessories/arg_parse.pyx"]),
+    Extension("BioMetaDB.Accessories.bio_ops", ["BioMetaDB/Accessories/bio_ops.pyx"]),
+    Extension("BioMetaDB.Accessories.ops", ["BioMetaDB/Accessories/ops.pyx"]),
+    Extension("BioMetaDB.Accessories.program_caller", ["BioMetaDB/Accessories/program_caller.pyx"]),
+    Extension("BioMetaDB.DataStructures.record_list", ["BioMetaDB/DataStructures/record_list.pyx"]),
+    Extension("BioMetaDB.DBManagers.class_manager", ["BioMetaDB/DBManagers/class_manager.pyx"]),
+    Extension("BioMetaDB.DBManagers.integrity_manager", ["BioMetaDB/DBManagers/integrity_manager.pyx"]),
+    Extension("BioMetaDB.DBManagers.update_manager", ["BioMetaDB/DBManagers/update_manager.pyx"]),
+    Extension("BioMetaDB.Models.functions", ["BioMetaDB/Models/functions.pyx"]),
+    Extension("BioMetaDB.Serializers.count_table", ["BioMetaDB/Serializers/count_table.pyx"]),
+    Extension("BioMetaDB.Serializers.fix_file", ["BioMetaDB/Serializers/fix_file.pyx"]),
+    Extension("BioMetaDB.Serializers.tsv_joiner", ["BioMetaDB/Serializers/tsv_joiner.pyx"]),
 ]
+
+if CYTHONIZE:
+    extensions = cythonize(extensions)
+else:
+    no_cythonize(extensions)
 
 setup(
     name='BioMetaDB',
